53f6714a52cc55301a46679e6ba808c918545132
nathanxluci4 <n29436384@gmail.com>
2025-02-08 12:11:15 -0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "MALVIN-XD~9pwWVApB#SLW2ezhKJRvoJjBPDlT9qUaIAzAxUx-u5uvqAKCiJy8",
 // add your Session Id 
 
 PREFIX: process.env.PREFIX || ".",
